71bff159cd1094f08324101015dbb4babb31d848
ptrickill <daniel.diaz.mosquera8@gmail.com>
2024-12-02 08:50:32 -0500

    Main Page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DiazDanielExamenProgreso2
+﻿using System.Data;
+
+namespace DiazDanielExamenProgreso2
 {
     public partial class MainPage : ContentPage
     {
@@ -7,7 +9,26 @@
             InitializeComponent();
         }
 
-        
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            string nombre = "Daniel";
+            string apellido = "Diaz";
+            string folderName = nombre + apellido;
+            string folerPath = Path.Combine(FileSystem.AppDataDirectory, folderName);
+
+            if (!Directory.Exists(folerPath))
+            {
+                Directory.CreateDirectory(folerPath);
+
+            }
+
+            string fileName = folderName + ".txt";
+            string filePath = Path.Combine(folerPath, fileName);
+            string recargaInfo = "Informacion de la recarga: $100.00\n Fecha: " + DateTime.Now;
+
+            await File.WriteAllLinesAsync(filePath, recargaInfo);
+            await DisplayAlert("Exito", "La infomrcion se ha guardado correctamente en " + filePath, "Ok");
+        }
     }
 
 }
--- Resources/StackLayout.xaml.cs
+++ Resources/StackLayout.xaml.cs
@@ -6,4 +6,34 @@ public partial class StackLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void JBtn_Recargar_Click(object sender, EventArgs e)
+	{
+		string numero = JTex_NumeroDDiaz.Text;
+		string nombre = JTex_Nombre.Text;
+
+		if (string.IsNullOrEmpty(numero) || string.IsNullOrEmpty(nombre))
+		{
+			await DisplayAlert("Error", "Por favor, ingrese todos los datos.", "OK");
+			return;
+
+		}
+
+		if (numero.Length != 10 || !long.TryParse(numero, out_))
+		{
+			await DisplayAlert("Error", "El numero de telefono debe tener 10 digitos validos", "Ok");
+			return;
+
+		}
+
+		string fileName = $"{nombre.Replace(" ","")}.txt";
+		string filePath = Path.Combine(FileSystem.AppDataDirectory,fileName);
+		File.WriteAllText(filePath, $"Nombre: {nombre}\nNumero: {numero}");
+		await DisplayAlert("Recarga Exitosa", "La recarga se realizo correctaente.", "Ok");
+		JLbl_ResumenDDiaz.Text = $"La ultima recarga realizada fue:\nNombre:{nombre}\nNumero:{numero}";
+		JTex_NumeroDDiaz.Text= string.Empty;
+		JTex_Nombre= string.Empty;
+
+
+	}
 }
